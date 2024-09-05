977aa2ae08d07de129f3966a660cd0c362015ce8
tom5454 <tom5454a@gmail.com>
2024-09-05 19:34:28 +0200

    1.5.3
    Fixed sample drill not dropping (Closes #113)

--- Fabric/src/main/java/com/tom/createores/data/COEDataGenerator.java
+++ Fabric/src/main/java/com/tom/createores/data/COEDataGenerator.java
@@ -1,6 +1,5 @@
 package com.tom.createores.data;
 
-import java.lang.reflect.Field;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -11,10 +10,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator.Pack;
 import net.minecraft.data.DataProvider;
 
 import com.simibubi.create.foundation.ponder.PonderLocalization;
-import com.tterrag.registrate.providers.ProviderType;
-import com.tterrag.registrate.providers.RegistrateDataProvider;
-
-import com.google.common.collect.BiMap;
 
 import com.tom.createores.CreateOreExcavation;
 import com.tom.createores.cc.TurtleUpgradeData;
@@ -26,14 +21,6 @@ public class COEDataGenerator implements DataGeneratorEntrypoint {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator generator) {
-		try {
-			Field f = RegistrateDataProvider.class.getDeclaredField("TYPES");
-			f.setAccessible(true);
-			BiMap<String, ProviderType<?>> map = (BiMap<String, ProviderType<?>>) f.get(null);
-			map.remove("loot");
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
 		Path[] paths = Arrays.stream(System.getProperty("porting_lib.datagen.existing_resources").split(";")).map(Paths::get).toArray(Path[]::new);
 		ExistingFileHelper helper = ExistingFileHelper.withResources(paths);
 		Pack pack = generator.createPack();
